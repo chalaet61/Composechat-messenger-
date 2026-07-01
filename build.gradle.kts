c6ffa26538f7139721e84f392672aae97ca43cbe
chalaet61 <chalaet61@gmail.com>
2026-07-01 05:00:24 +0300

    build: configure debug signing and add APK tasks
    - Configure debug signing config in app build file.
    - Add Gradle tasks to copy and verify debug APK output.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,32 @@ plugins {
   alias(libs.plugins.secrets) apply false
   alias(libs.plugins.google.services) apply false
 }
+
+tasks.register<Copy>("copyApkToDownload") {
+    from(file("${project.rootDir}/.build-outputs/app-debug.apk"))
+    into(file("${project.rootDir}/APK_DOWNLOAD"))
+}
+
+val rootDirFile = project.rootDir
+
+tasks.register("verifyApkDetails") {
+    val apkFile = File(rootDirFile, "APK_DOWNLOAD/app-debug.apk")
+    doLast {
+        if (apkFile.exists()) {
+            val sizeInBytes = apkFile.length()
+            val sizeInMb = sizeInBytes.toDouble() / (1024 * 1024)
+            println("APK File exists at: ${apkFile.absolutePath}")
+            println("APK File size: $sizeInBytes bytes ($sizeInMb MB)")
+            if (sizeInBytes > 1024 * 1024) {
+                println("APK verified successfully: Size is greater than 1 MB.")
+            } else {
+                throw GradleException("APK size is less than 1 MB!")
+            }
+        } else {
+            throw GradleException("APK file does not exist!")
+        }
+    }
+}
+
+
+
